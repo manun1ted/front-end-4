468531daefea75b8116a6975c611ab0232008faf
Oleksii Oseledko <oseledko.o@gmail.com>
2017-11-19 22:51:41 +0200

    Lesson-17

--- homework/javascript/jscript.js
+++ homework/javascript/jscript.js
@@ -16,14 +16,36 @@ function sumData(array) {
     } else return false;
 }
 
-/---------------------------/;
+//---------------------------//
 
-var codeObj = [45, 109, 32, 39, 1, 0];
-codeObj[23]=95;
-function sumData(codeObj) {
-    sumData = 0;
-    for (var key in codeObj.length){
-        sumData = sumData + codeObj[i];
+var codeObj = {
+   'length': [45, 109, 32, 39, 1, 0]
+};
+codeObj.length[23]=95;
+function sumD(codeObj) {
+    sumD = 0;
+    for (var i in codeObj) {
+        sumD = sumD + codeObj[i];
     }
-    return sumData;
+    return sumD;
 }
+//Тут якась фігня. Замість суми мені виводить сам масив і до першого числа дописує 0//
+
+
+
+var data = [1, 2, 7, 10, 6, 9, 3, 8, 5, 4];
+function sortBubble(data) {
+    for (var y = 0; y < data.length; y++) {
+        for (var i = 0; data.length - 1; i++) {
+            if (data[i] > data[i + 1]) {
+                var x = data[i];
+                data[i] = data[i + 1];
+                data[i + 1] = x;
+            }
+        }
+
+    }
+    return data;
+}
+
+//Тут ніби логіка правильна, але працювати не хоче//
